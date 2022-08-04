caf41332b4934b37e512a0c5a1e55e67ed5c86e0
David <Rigby335@hotmail.com>
2022-08-04 14:21:48 -0700

    test get /discs setup (fails)

---
+++ __tests__/discs.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const { request } = require('express');
+// const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('#GET /discs should return a disc with id, name, type, speed, glide', async () => {
+    const res = await request(app).get('/discs');
+    expect(res.body.length).toBe(6);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      type: expect.any(String),
+      speed: expect.any(Number),
+      glide: expect.any(Number),
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,27 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS discs;
+
+CREATE TABLE discs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    type VARCHAR,
+    speed INT,
+    glide INT
+)
+
+INSERT INTO discs (
+    name,
+    type,
+    speed,
+    glide
+)
+
+VALUES
+    ('Wraith', 'Driver', 11, 5),
+    ('Whale', 'Putter', 2, 3),
+    ('Buzzz', 'Midrange', 5, 4),
+    ('Nuke', 'Driver', 13, 5),
+    ('Explorer', 'Driver', 7, 5),
+    ('Raptor', 'Driver', 9, 4);
