c49908d2fbc23e16562492689a36d2d9d0e61c2d
David <Rigby335@hotmail.com>
2022-08-04 17:07:01 -0700

    #GET /pros/:id passes

--- __tests__/discs.test.js
+++ __tests__/discs.test.js
@@ -19,7 +19,7 @@ describe('backend-express-template routes', () => {
       glide: expect.any(Number),
     });
   });
-  it('#GET /discs:id should return a specific disc', async () => {
+  it('#GET /discs/:id should return a specific disc', async () => {
     const res = await request(app).get('/discs/1');
     expect(res.body).toEqual({
       id: '1',
--- __tests__/pros.test.js
+++ __tests__/pros.test.js
@@ -18,6 +18,16 @@ describe('backend-express-template routes', () => {
       dob: expect.any(Number),
     });
   });
+  it('#GET /pros/:id should return a specific pro', async () => {
+    const res = await request(app).get('/pros/1');
+    console.log(res.body);
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'Paul McBeth',
+      pob: 'California',
+      dob: 1990
+    });
+  });
   afterAll(() => {
     pool.end();
   });
